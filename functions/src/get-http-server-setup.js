0d551a342cea80a6c7d049bdf2ca75386881968a
rmamidal <rajendhar.mamidala@capgemini.com>
2017-11-03 00:52:30 +0530

    Integrated server

---
+++ functions/src/get-http-server-setup.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const getHTTPHandler = require('./http-handler');
+const setupSIG = require('./setup-sigint-sigterm');
+
+module.exports = function getHTTPServerSetup(tws, handlers) {
+  const that = tws;
+  return function setup(modelID) {
+    that.config.modelID = modelID;
+
+    let path = that.config.hostURL;
+    if (path.substr(0, -1) !== '/') {
+      path += '/';
+    }
+    that.config.callbackURL = `${path}trelloUserWebhook`;
+    path = that.config.callbackURL.match(/https?:\/\/[^\/]+(\/.*)/)[1];
+
+    const httpHandler = getHTTPHandler(tws, handlers);
+
+    const realHandler = function realHandler(req, res) {
+      if (req.url === path) {
+        httpHandler(req, res);
+      }
+    };
+
+    //that.config.server.on('request', realHandler);
+
+    setupSIG(that.registrar);
+    console.log("callback url "+ that.config.callbackURL);
+    console.log("modelID "+ modelID);
+    return that.registrar.register(that.config.callbackURL, modelID);
+  };
+};
